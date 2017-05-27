52aa8f76ad4f96e1d8869823ed52f597a9797caa
andrewmoon1 <andrew.moon1@yahoo.com>
2017-05-26 21:08:23 -0500

    config changes, added session secret to dotenv

--- config/secrets.js
+++ config/secrets.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-export const sessionSecret = process.env.SESSION_SECRET || 'Your Session Secret goes here';
+export const sessionSecret = process.env.SESSION_SECRET;
 export const google = {
   clientID: process.env.GOOGLE_CLIENTID,
   clientSecret: process.env.GOOGLE_SECRET,
