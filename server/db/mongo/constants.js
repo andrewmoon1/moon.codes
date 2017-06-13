86b226a1e5437143de77868937746641c6e6d121
andrewmoon1 <andrew.moon1@yahoo.com>
2017-06-13 17:07:39 -0500

    adding authentication to db

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,4 +1,6 @@
-export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://localhost/ReactWebpackNode';
+require('dotenv').config();
+
+export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost/ReactWebpackNode`;
 
 export default {
   db
