566e1230088ec68bbdbf610a351aa1d1948c4655
andrewmoon1 <andrew.moon1@yahoo.com>
2017-06-14 14:36:44 -0500

    db auth

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -3,19 +3,21 @@ const fs = require('file-system');
 
 export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost/ReactWebpackNode`;
 
-const key = fs.readFileSync(process.env.KEY);
-const ca = [fs.readFileSync(process.env.CA)];
-
+const key = process.env.KEY ? fs.readFileSync(process.env.KEY) : '';
+const ca = process.env.CA ? [fs.readFileSync(process.env.CA)] : '';
+const server = process.env.CA ?
+  {
+      sslValidate: true,
+      sslCA: ca,
+      sslKey: key,
+      sslCert: key,
+      ssl: true
+  } :
+  {};
 export const options = {
   user: process.env.DB_USER,
   pass: process.env.DB_PASS,
-  server: {
-        sslValidate: true,
-        sslCA: ca,
-        sslKey: key,
-        sslCert: key,
-        ssl: true
-    }
+  server
 };
 
 export default {
