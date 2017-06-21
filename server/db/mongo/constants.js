68f83bb140e8ae26ca126cb78cfde68468b5002f
andrewmoon1 <andrew.moon1@yahoo.com>
2017-06-21 17:36:16 -0500

    fixed db string

--- server/db/mongo/constants.js
+++ server/db/mongo/constants.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const fs = require('file-system');
 
-export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@www.moon.codes/ReactWebpackNode`;
+export const db = process.env.MONGOHQ_URL || process.env.MONGODB_URI || `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost/ReactWebpackNode`;
 
 const key = process.env.KEY ? fs.readFileSync(process.env.KEY) : '';
 const ca = process.env.CA ? [fs.readFileSync(process.env.CA)] : '';
